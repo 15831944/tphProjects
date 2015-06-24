66f7ee57d74d9255e68f646cd8fcecb94630d2cf
tangpinghui <tangpinghui@pset.suntec.net>
2015-06-24 12:15:55 +0800

    fix conflict

--- Level2/Level3/vs2003final/src/Main/ComparativePlot.h
+++ Level2/Level3/vs2003final/src/Main/ComparativePlot.h
@@ -1,7 +1,6 @@
 #pragma once
 #include "../compare/Cmp3DChartWnd.h"
 #include "../compare/ThreeDChartType.h"
-#include "../AirsideReport/CARC3DChart.h"
 
 class CComparativeReportResult;
 class CComparativeQLengthReport;
@@ -42,37 +41,6 @@ private:
 
 	ThreeDChartType m_iType;
 	N3DCHARTLib::IDualChartPtr& m_pChart;
-	const std::vector<CModelToCompare*>& m_vModelList;
-};
-
-class CComparativePlot_new
-{
-public:
-	CComparativePlot_new(ThreeDChartType iType,	CARC3DChart& pChart, const std::vector<CModelToCompare*>& vModelList)
-		: m_iType(iType)
-		, m_pChart(pChart)
-		, m_vModelList(vModelList)
-	{
-	}
-
-	bool Draw3DChart(const CComparativeReportResult& _reportData);
-	bool Update3DChart(ThreeDChartType iType);
-
-private:
-	void Init3DChart();
-	void UniformAppearances();
-
-	bool Draw3DChart(const CComparativeQTimeReport& _reportData);
-	bool Draw3DChart(const CComparativeQLengthReport& _reportData);
-	bool Draw3DChart(const CComparativeThroughputReport& _reportData);
-	bool Draw3DChart(const CComparativeSpaceDensityReport& _reportData);
-	bool Draw3DChart(const CComparativePaxCountReport& _reportData);
-	bool Draw3DChart(const CComparativeAcOperationReport& _reportData);
-	bool Draw3DChart(const CComparativeTimeTerminalReport& _reportData);
-	bool Draw3DChart(const CComparativeDistanceTravelReport & _reportData);
-
-	ThreeDChartType m_iType;
-	CARC3DChart& m_pChart;
 	const std::vector<CModelToCompare*>& m_vModelList;
 };
 
