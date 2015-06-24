b11583707b1166577187b8ec1c54a43dd1ae106c
tangpinghui <tangpinghui@pset.suntec.net>
2015-06-24 12:06:23 +0800

    compare report

--- Level2/Level3/vs2003final/src/compare/CmpParametersWnd.h
+++ Level2/Level3/vs2003final/src/compare/CmpParametersWnd.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include ".\MFCExControl\CoolTree.h"
+
 class CCmpReportManager;
 
 class CCmpParametersWnd : public CWnd
@@ -20,9 +22,11 @@ public:
 	}
 	void InitParaWnd();
 	void UpdateParaWnd();
+	virtual LRESULT DefWindowProc(UINT message, WPARAM wParam, LPARAM lParam);
 protected:
 	DECLARE_MESSAGE_MAP()
 	CXTPPropertyGrid m_wndPropGrid;
+	CCoolTree m_treex;
 	CButton m_btnRun;
 	CButton m_btnCancel;
 	CCmpReportManager* m_pReportManager;
@@ -41,6 +45,11 @@ protected:
 	CXTPPropertyGridItem* m_pItemProcessor;
 	CXTPPropertyGridItem* m_pItemSpace;
 
+	HTREEITEM m_hBasicInfo;
+	HTREEITEM m_hProjName;
+	HTREEITEM m_hProjDesc;
+	HTREEITEM m_hModelRoot;
+	HTREEITEM m_hReportRoot;
 
 	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
 	afx_msg void OnSize(UINT nType, int cx, int cy);
@@ -53,18 +62,26 @@ protected:
 	afx_msg void OnCrcAddNewReport();
 	afx_msg void OnCrdEditReport();
 	afx_msg void OnCrdDelete();
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	afx_msg void OnChooseMenu(UINT nID);
 
 
 	CString GetRegularDateTime(LPCTSTR elaptimestr, bool needsec = true);
 	void RunCompareReport();
 	BOOL CheckData();
+	void OnCrdDelete1();
+	void OnCrdEditReport1();
 	void RemoveItem(CXTPPropertyGridItem* pItem);
 	void UpdateParaItem(CXTPPropertyGridItem* pItem);
-	
+	void RemoveSubItem(HTREEITEM pItem);
+	void UpdateParaItem(HTREEITEM pItem);
+
 	CMenu m_nMenu, *m_pSubMenu;
 
 public:
 	afx_msg void OnTimer(UINT nIDEvent);
+private:
+	void InitNodeInfo(CWnd* pParent,COOLTREE_NODE_INFO& CNI,BOOL bVerify=TRUE);
 };
 
 
