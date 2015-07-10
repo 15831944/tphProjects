d38914a2a817f94f69c5bc3bba373003af8085e2
tangpinghui <tangpinghui@pset.suntec.net>
2015-07-10 10:17:14 +0800

    tomtom sa: spotguide estimation.

--- Suntec/PicsCombine/tomtom_south_africa_spotguide_estimation.py
+++ Suntec/PicsCombine/tomtom_south_africa_spotguide_estimation.py
@@ -1,6 +1,7 @@
 # -*- coding: cp936 -*-
 import psycopg2
 
+maximum_lane_count = 10
 class IllustDataGenerator(object):
     def __init__(self, dbIP, dbName, userName, password):
         self.conn = psycopg2.connect(''' host='%s' dbname='%s' user='%s' password='%s' ''' % \
@@ -9,7 +10,8 @@ class IllustDataGenerator(object):
     
     def generateSpotguidePointLatLon(self, outputCSV):
         sqlcmd = '''
-                    select inlinkid, lat_lon, inlink_lane_count, tolink_list, from_list, to_list
+                    select inlinkid, lat_lon, inlink_lane_count, tolink_list, from_list, to_list,
+                            outlinkid_list
                     from temp_target_table
                 '''
         self.pg.execute(sqlcmd)
@@ -18,13 +20,14 @@ class IllustDataGenerator(object):
         oFStream = open(outputCSV, 'w')
         for row in rows:
             inlinkid = row[0]
-            if inlinkid == -1:
+            if inlinkid is None:
                 continue
             lat_lon = row[1]
             inlink_lane_count = row[2]
             tolink_list = row[3]
             from_list = row[4]
             to_list = row[5]
+            outlinkid_list = row[6]
             
             
             if inlink_lane_count is None:
@@ -33,30 +36,25 @@ class IllustDataGenerator(object):
                     if (int(oneFrom) > inlink_lane_count):
                         inlink_lane_count = int(oneFrom)
             line.append(inlink_lane_count) # 流入link的lane数
-            laneDict = {1:[],2:[],3:[],4:[],5:[],6:[],7:[],8:[], 9:[]}           
+            laneDict = {}
+            for i in range(1, maximum_lane_count+1):
+                laneDict[i] = []          
             for i in range(0, len(tolink_list)): # tolink_list, from_list, to_list的长度必定相等。
                 laneDict[int(from_list[i])].append('''%d/%s''' % (tolink_list[i], to_list[i]))
             
             tolinkDict = {}
+            for oneOutlinkid in outlinkid_list:
+                tolinkDict[oneOutlinkid] = []
             for i in range(0, len(tolink_list)): # tolink_list, from_list, to_list的长度必定相等。
-                if tolinkDict.has_key(tolink_list[i]) == False:
-                    tolinkDict[tolink_list[i]] = []
                 tolinkDict[tolink_list[i]].append(to_list[i])
                 
             line = []
             line.append('''%d''' % inlinkid)
             line.append(lat_lon)
             line.append('''%d''' % inlink_lane_count)
-            line.append(';'.join(laneDict[1]))
-            line.append(';'.join(laneDict[2]))
-            line.append(';'.join(laneDict[3]))
-            line.append(';'.join(laneDict[4]))
-            line.append(';'.join(laneDict[5]))
-            line.append(';'.join(laneDict[6]))
-            line.append(';'.join(laneDict[7]))
-            line.append(';'.join(laneDict[8]))
-            line.append(';'.join(laneDict[9]))
-            line.append('''%d''' % len(tolinkDict))
+            for i in range(1, maximum_lane_count + 1):
+                line.append('''"%s"''' % chr(0x0a).join(laneDict[i]))
+            line.append('''%d''' % len(outlinkid_list))
             for tolinkid, tolinkLanelist in tolinkDict.items():
                 str1 = '''%d/%s''' % (tolinkid, '-'.join(tolinkLanelist))
                 line.append(str1)
--- Suntec/PicsCombine/tomtom_south_africa_spotguide_estimation.sql
+++ Suntec/PicsCombine/tomtom_south_africa_spotguide_estimation.sql
@@ -67,27 +67,29 @@ BEGIN
 END;
 $$;
 
--- 表4：将表2按车线连接点id分组。
+-- 表4：将表2按jnctid分组。
 drop table if exists org_ln_with_link_information_grouped;
-select id, fromto, jnctid, get_inlink_id(array_agg(seqnr), array_agg(trpelid)) as inlinkid, 
-	get_tolink_id(array_agg(seqnr), array_agg(trpelid)) as tolinkid
+select id, jnctid, get_inlink_id(array_agg(seqnr), array_agg(trpelid)) as inlinkid, 
+	get_tolink_id(array_agg(seqnr), array_agg(trpelid)) as tolinkid,
+	substr(fromto, 1, position('/' in fromto)-1) as from_lane, 
+	substr(fromto, position('/' in fromto)+1, length(fromto)) as to_lane
 into org_ln_with_link_information_grouped
 from org_ln_with_link_information
 group by id, fromto, jnctid;
 
--- 表5：根据表1和表4求出所有highway signpost的车线转向信息。
+-- 表5：表1连表4求出所有highway signpost的车线转向信息。
 drop table if exists temp_sign_and_its_arrow_dir;
-select a.id as sign_id, a.jnctid as sign_jnctid, a.the_geom, b.id as lane_conn_id, b.fromto, b.jnctid as lane_conn_jnctid,
-	b.inlinkid, b.tolinkid, to_char(st_y(a.the_geom), '999D999999') || ' ' || to_char(st_x(a.the_geom), '999D999999') as lat_lon
+select a.id as sign_id, a.jnctid, a.the_geom, b.id as lane_conn_id, b.inlinkid, b.tolinkid, b.from_lane, b.to_lane,
+	to_char(st_y(a.the_geom), '999D999999') || ' ' || to_char(st_x(a.the_geom), '999D999999') as lat_lon
 into temp_sign_and_its_arrow_dir
 from
 org_sg_highway_only as a
 left join org_ln_with_link_information_grouped as b
 on a.jnctid=b.jnctid and a.inlink_id=b.inlinkid;
 
--- 表6：使用表4连接org_ld表，求出vilidity以计算inlink的lane数
+-- 表6：使用表4连接org_ld表，根据org_ld.vilidity求出inlink的车线数。
 drop table if exists temp_sign_and_its_arrow_dir_vilidity;
-select distinct a.inlinkid, a.tolinkid, a.fromto, a.lat_lon, b.validity as inlink_validity
+select distinct a.inlinkid, a.jnctid, a.tolinkid, a.from_lane, a.to_lane, a.lat_lon, length(b.validity)-1 as inlink_lane_count
 into temp_sign_and_its_arrow_dir_vilidity
 from
 temp_sign_and_its_arrow_dir as a
@@ -95,9 +97,53 @@ left join org_ld as b
 on a.inlinkid=b.id;
 
 -- 表7：target table
+drop table if exists temp_target_table1;
+select a.inlinkid, a.jnctid, a.inlink_lane_count, a.lat_lon, array_agg(tolinkid) as tolink_list, 
+	array_agg(from_lane) as from_list, 
+	array_agg(to_lane) as to_list
+into temp_target_table1
+from 
+temp_sign_and_its_arrow_dir_vilidity as a
+group by a.inlinkid, a.jnctid, a.inlink_lane_count, a.lat_lon;
+
+-- 表8：表7连org_nw取得该highway-signpost点关联的所有outlink信息。
+drop table if exists temp_highway_signpost_jnct_with_outlinkid_list;
+select d.inlinkid, d.lat_lon, array_agg(d.outlinkid) as outlinkid_list
+into temp_highway_signpost_jnct_with_outlinkid_list
+from
+(
+select a.inlinkid, a.lat_lon, b.id as outlinkid
+from temp_target_table1 as a
+left join org_nw as b
+on (a.jnctid=b.f_jnctid and (b.oneway='FT' or b.oneway is null)) 
+where b.id is not null and a.inlinkid<>b.id
+union 
+select a.inlinkid, a.lat_lon, b.id as outlinkid
+from temp_target_table1 as a
+left join org_nw as b
+on (a.jnctid=b.t_jnctid and (b.oneway='TF' or b.oneway is null))
+where b.id is not null and a.inlinkid<>b.id
+) as d
+group by d.inlinkid, d.lat_lon;
+
+-- 表9：表7连表8.
 drop table if exists temp_target_table;
-select inlinkid, lat_lon, length(inlink_validity) as inlink_lane_count, array_agg(tolinkid) as tolink_list, 
-	array_agg(substr(fromto, 1, 1)) as from_list, array_agg(substr(fromto, 3, 1)) as to_list
+select a.*, b.outlinkid_list
 into temp_target_table
-from temp_sign_and_its_arrow_dir_vilidity 
-group by inlinkid, length(inlink_validity), lat_lon
+from temp_target_table1 as a
+left join temp_highway_signpost_jnct_with_outlinkid_list as b
+on a.inlinkid=b.inlinkid;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
