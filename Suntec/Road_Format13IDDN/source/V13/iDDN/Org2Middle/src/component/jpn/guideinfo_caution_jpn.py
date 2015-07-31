48a4bc1bca54f27f123392fa448ac5f22bc9767d
tangpinghui <tangpinghui@pset.suntec.net>
2015-07-31 14:21:10 +0800

    refactor caution_jpn code, version1

--- Suntec/Road_Format13IDDN/source/V13/iDDN/Org2Middle/src/component/jpn/guideinfo_caution_jpn.py
+++ Suntec/Road_Format13IDDN/source/V13/iDDN/Org2Middle/src/component/jpn/guideinfo_caution_jpn.py
@@ -1,13 +1,10 @@
 '''
 Created on 2012-2-23
-
 @author: sunyifeng
 '''
-
 import common
 import base
 import io
-import os
 
 class comp_guideinfo_caution_jpn(base.component_base.comp_base):
     
@@ -21,15 +18,15 @@ class comp_guideinfo_caution_jpn(base.component_base.comp_base):
         
         self.CreateTable2('caution_tbl')
         
-        self.CreateTable2('temp_boundary')
-        sqlcmd = 'Alter table temp_boundary drop CONSTRAINT enforce_geotype_the_geom;'
+        self.CreateTable2('temp_admin_province_boundary')
+        sqlcmd = 'Alter table temp_admin_province_boundary drop CONSTRAINT enforce_geotype_the_geom;'
         self.pg.execute2(sqlcmd)
         self.pg.commit2()
         
-        self.CreateTable2('temp_hlink')
+        self.CreateTable2('temp_highway_link')
         
-        self.CreateTable2('temp_inode')
-        sqlcmd = 'Alter table temp_inode drop CONSTRAINT enforce_geotype_the_geom;'
+        self.CreateTable2('temp_link_intersection_points')
+        sqlcmd = 'Alter table temp_link_intersection_points drop CONSTRAINT enforce_geotype_the_geom;'
         self.pg.execute2(sqlcmd)
         self.pg.commit2()
         
@@ -66,101 +63,78 @@ class comp_guideinfo_caution_jpn(base.component_base.comp_base):
         f.close()
         
         sqlcmd = """
-            INSERT into temp_boundary(ad_cd, the_geom)
-                SELECT ad_cd, ST_Boundary(the_geom) as the_geom
-                  FROM rdb_admin_province;
+            insert into temp_admin_province_boundary(ad_cd, the_geom)
+            select ad_cd, ST_Boundary(the_geom) as the_geom
+            from rdb_admin_province;
         """
         
         self.pg.execute2(sqlcmd)
         self.pg.commit2()
         
         sqlcmd = """
-            INSERT INTO temp_hlink(link_id, s_node, e_node, one_way_code, the_geom)
-                SELECT link_id, s_node, e_node, one_way_code, the_geom
-                  FROM link_tbl
-                  where road_type in (0,1) and link_type = 2;
+            insert into temp_highway_link(link_id, the_geom)
+            select link_id, the_geom
+            from link_tbl
+            where road_type in (0,1) and link_type = 2;
         """   
         
         self.pg.execute2(sqlcmd)
         self.pg.commit2() 
         
-        self.CreateIndex2('temp_boundary_the_geom_idx')
-        self.CreateIndex2('temp_hlink_the_geom_idx')
+        self.CreateIndex2('temp_admin_province_boundary_the_geom_idx')
+        self.CreateIndex2('temp_highway_link_the_geom_idx')
 
 
         sqlcmd = """
-            INSERT INTO temp_inode(link_id, s_node, e_node, one_way_code, the_geom)
-                SELECT distinct a.link_id, a.s_node, a.e_node, a.one_way_code,
-                        ST_Intersection(a.the_geom, b.the_geom) as the_geom
-                    FROM temp_hlink as a
-                    inner join temp_boundary as b
-                    on ST_Intersects(a.the_geom, b.the_geom) = TRUE
+            insert into temp_link_intersection_points(link_id, the_intersec_points_geom)
+            select distinct a.link_id, a.s_node, a.e_node, a.one_way_code,
+                   ST_Intersection(a.the_geom, b.the_geom) as the_intersec_points_geom
+            from temp_highway_link as a
+            inner join temp_admin_province_boundary as b
+            on ST_Intersects(a.the_geom, b.the_geom) = TRUE
         """ 
         
         self.pg.execute2(sqlcmd)
         self.pg.commit2()  
 
         sqlcmd = """
-            INSERT INTO temp_bnode(link_id, s_node, e_node, one_way_code, bnode, 
-                                    link_id2, s_node2, e_node2, one_way_code2, the_geom)
-                SELECT t1.link_id, t1.s_node, t1.e_node, t1.one_way_code, t1.bnode, 
-                        t2.link_id, t2.s_node, t2.e_node, t2.one_way_code, t1.the_geom
-                    FROM 
-                    (
-                        SELECT link_id, s_node, e_node, one_way_code,
-                            case when slocate < 0.5 and slocate < 1 - elocate then s_node else e_node end as bnode,
-                            case when slocate < 0.5 and slocate < 1 - elocate then sp else ep end as the_geom,
-                            slocate,
-                            elocate
-                        FROM
-                        (
-                        SELECT a.link_id, a.s_node, a.e_node, a.one_way_code, 
-                            ST_Line_Locate_Point(the_geom, sbp) as slocate,
-                            ST_Line_Locate_Point(the_geom, ebp) as elocate,
-                            sp,
-                            ep
-                            FROM 
-                              (
-                              select m.link_id, m.s_node, m.e_node, m.one_way_code, n.the_geom, 
-                                    ST_ClosestPoint(m.the_geom, ST_StartPoint(n.the_geom)) as sbp,
-                                    ST_ClosestPoint(m.the_geom, ST_EndPoint(n.the_geom)) as ebp,
-                                    ST_StartPoint(n.the_geom) as sp,
-                                    ST_EndPoint(n.the_geom) as ep
-                                from temp_inode as m
-                                inner join link_tbl as n
-                                on m.link_id = n.link_id
-                              ) as a
-                        ) as t
-                    ) as t1
-                    inner join link_tbl as t2
-                    on t1.bnode = t2.s_node or t1.bnode = t2.e_node
-                    where t1.link_id != t2.link_id;
+            insert into temp_bnode(link_id, bnode, link_id2, the_geom)
+            select t1.link_id, t1.bnode, t2.link_id, t1.the_geom
+            from 
+            (
+                select m.link_id, m.the_geom as m_the_geom, n.the_geom as n_the_geom
+                from 
+                temp_link_intersection_points as m
+                inner join link_tbl as n
+                on m.link_id = n.link_id
+            ) as t1
+            inner join link_tbl as t2
+            on t1.bnode = t2.s_node or t1.bnode = t2.e_node
+            where t1.link_id != t2.link_id;
         """  
         
         self.pg.execute2(sqlcmd)
         self.pg.commit2()
         
         sqlcmd = """
-            INSERT INTO temp_guideinfo_boundary(inlinkid, nodeid, outlinkid, innode, outnode, out_adcd)
-                SELECT distinct inlinkid, bnode, outlinkid, innode, outnode, b.ad_cd
+            insert into temp_guideinfo_boundary(inlinkid, nodeid, outlinkid, innode, outnode, out_adcd)
+            select distinct inlinkid, bnode, outlinkid, innode, outnode, b.ad_cd
+            from
+            (
+                select links[1] as inlinkid, bnode, links[2] as outlinkid, 
+                       links[3] as innode, links[4] as outnode
                 from
-                (
-                    SELECT links[1] as inlinkid, bnode, links[2] as outlinkid, 
-                        links[3] as innode, links[4] as outnode
-                    from
-                    ( 
-                    SELECT 
-                        bnode,
-                        mid_get_inout_link(link_id, s_node, e_node, one_way_code, 
-                                link_id2, s_node2, e_node2, one_way_code2) as links
-                        from temp_bnode
-                    ) as t
-                    where links is not null
-                ) as a
-                inner join node_tbl as c
-                on a.outnode = c.node_id
-                inner join rdb_admin_province as b
-                on ST_Intersects(c.the_geom, b.the_geom) = TRUE;
+                ( 
+                    select bnode, mid_get_inout_link(link_id, s_node, e_node, one_way_code, 
+                           link_id2, s_node2, e_node2, one_way_code2) as links
+                    from temp_bnode
+                ) as t
+                where links is not null
+            ) as a
+            inner join node_tbl as c
+            on a.outnode = c.node_id
+            inner join rdb_admin_province as b
+            on ST_Intersects(c.the_geom, b.the_geom) = TRUE;
         """  
         
         self.pg.execute2(sqlcmd)
