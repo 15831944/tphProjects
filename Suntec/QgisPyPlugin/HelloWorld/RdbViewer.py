1b7842d42ac4b642ec934365cd7d1f62dab41eb4
tangpinghui <tangpinghui@pset.suntec.net>
2015-09-28 16:50:44 +0800

    mark: file structure refactor ok.

--- Suntec/QgisPyPlugin/HelloWorld/RdbViewer.py
+++ Suntec/QgisPyPlugin/HelloWorld/RdbViewer.py
@@ -9,96 +9,51 @@ import os.path
 class RdbViewer:
     def __init__(self, iface):
         self.iface = iface
-        self.plugin_dir = os.path.dirname(__file__)
-        locale = QSettings().value('locale/userLocale')[0:2]
-        locale_path = os.path.join(
-            self.plugin_dir,
-            'i18n',
-            'RdbViewer_{}.qm'.format(locale))
-
-        if os.path.exists(locale_path):
-            self.translator = QTranslator()
-            self.translator.load(locale_path)
-            if qVersion() > '4.3.3':
-                QCoreApplication.installTranslator(self.translator)
-
         self.dlg = GenerateNewLayerDlg()
         self.actions = []
-        self.menu = self.tr(u'&RdbViewer0')
-        self.toolbar = self.iface.addToolBar(u'RdbViewer1')
-        self.toolbar.setObjectName(u'RdbViewer2')
-
-    def tr(self, message):
-        return QCoreApplication.translate('RdbViewer3', message)
-
-
-    def add_action(
-        self,
-        icon_path,
-        text,
-        callback,
-        enabled_flag=True,
-        add_to_menu=True,
-        add_to_toolbar=True,
-        status_tip=None,
-        whats_this=None,
-        parent=None):
-        icon = QIcon(icon_path)
-        action = QAction(icon, text, parent)
-        action.triggered.connect(callback)
-        action.setEnabled(enabled_flag)
-
-        if status_tip is not None:
-            action.setStatusTip(status_tip)
-
-        if whats_this is not None:
-            action.setWhatsThis(whats_this)
-
-        if add_to_toolbar:
-            self.toolbar.addAction(action)
-
-        if add_to_menu:
-            self.iface.addPluginToMenu(
-                self.menu,
-                action)
-
-        self.actions.append(action)
-        return action
+        self.menu = u'&Rdb Viewer'
+        self.toolbar = self.iface.addToolBar(u'RdbViewerToolbar')
+        self.toolbar.setObjectName(u'RdbViewerToolbar')
 
     def initGui(self):
-        icon_path = ':/plugins/NearestFeature/icon.png'       
-        action = self.add_action(
-                    icon_path,
-                    text=self.tr(u'Select nearest feature.'),
-                    callback=self.run,
-                    parent=self.iface.mainWindow())
-        
-        action.setCheckable(True)
-        # Create a new NearestFeatureMapTool and keep reference
-        self.nearestFeatureMapTool = SpotguideMapTool(self.iface.mapCanvas())
-        self.nearestFeatureMapTool.setAction(action)
-
-        actionGenerateLayer = self.add_action(
-                    icon_path, 
-                    text=self.tr(u'Generate a special layer.'),
-                    callback = self.generateLayer,
-                    parent = self.iface.mainWindow())
+        icon_path = ':/plugins/NearestFeature/icon.png' 
+        actionGenerateLayer = QAction(QIcon(icon_path), u"Generate Layer", None)
+        actionGenerateLayer.triggered.connect(self.generateLayer)
+        actionGenerateLayer.setStatusTip(None)
+        actionGenerateLayer.setWhatsThis(None)
+        self.iface.addPluginToMenu(self.menu, actionGenerateLayer)
+        actionGenerateLayer.setEnabled(True)
         actionGenerateLayer.setCheckable(True)
+        self.actions.append(actionGenerateLayer)
+
+
+        icon_path = ':/plugins/NearestFeature/icon.png' 
+        actionShowSpotguide = QAction(QIcon(icon_path), u"Show Spotguide", None)
+        actionShowSpotguide.triggered.connect(self.run)
+        actionShowSpotguide.setStatusTip(None)
+        actionShowSpotguide.setWhatsThis(None)
+        self.toolbar.addAction(actionShowSpotguide)
+        self.iface.addPluginToMenu(self.menu, actionShowSpotguide)
+        actionShowSpotguide.setEnabled(True)
+        actionShowSpotguide.setCheckable(True)
+        self.actions.append(actionShowSpotguide)
+
+        self.spotguideMapTool = SpotguideMapTool(self.iface.mapCanvas())
+        self.spotguideMapTool.setAction(actionShowSpotguide)
+        return
 
     def unload(self):
         for action in self.actions:
-            self.iface.removePluginMenu(
-                self.tr(u'&Nearest Feature'),
-                action)
+            self.iface.removePluginMenu(None, action)
             self.iface.removeToolBarIcon(action)
         
         # Unset the map tool in case it's set
-        self.iface.mapCanvas().unsetMapTool(self.nearestFeatureMapTool)
+        self.iface.mapCanvas().unsetMapTool(self.spotguideMapTool)
         return
 
     def run(self):
         # activate our tool
-        self.iface.mapCanvas().setMapTool(self.nearestFeatureMapTool)
+        self.iface.mapCanvas().setMapTool(self.spotguideMapTool)
         return
 
     def generateLayer(self):
