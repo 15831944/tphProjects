5a9b234da6b6089cb1c23d7bc9ce277dc4ceb3fa
tangpinghui <tangpinghui@pset.suntec.net>
2015-10-10 15:18:55 +0800

    lane with some bugs

--- Suntec/QgisPyPlugin/RdbViewer/LaneShowImageDlg.py
+++ Suntec/QgisPyPlugin/RdbViewer/LaneShowImageDlg.py
@@ -23,27 +23,27 @@ class LaneShowImageDlg(QtGui.QDialog, FORM_CLASS):
         self.mTheLayer = theLayer
         self.mFeatureList = selectedFeatureList
         # 
-        self.labelsMap = {
-            2**0 : self.label_0,
-            2**1 : self.label_1,
-            2**2 : self.label_2,
-            2**3 : self.label_3,
-            2**4 : self.label_4,
-            2**5 : self.label_5,
-            2**6 : self.label_6,
-            2**7 : self.label_7,
-            2**8 : self.label_8,
-            2**9 : self.label_9,
-            2**10 : self.label_10,
-            2**11 : self.label_11,
-            2**12 : self.label_12,
-            2**13 : self.label_13,
-            2**14 : self.label_14,
-            2**15 : self.label_15
+        self.graphicViewsMap = {
+            0 : self.graphicView_0,
+            1 : self.graphicView_1,
+            2 : self.graphicView_2,
+            3 : self.graphicView_3,
+            4 : self.graphicView_4,
+            5 : self.graphicView_5,
+            6 : self.graphicView_6,
+            7 : self.graphicView_7,
+            8 : self.graphicView_8,
+            9 : self.graphicView_9,
+            10 : self.graphicView_10,
+            11 : self.graphicView_11,
+            12 : self.graphicView_12,
+            13 : self.graphicView_13,
+            14 : self.graphicView_14,
+            15 : self.graphicView_15
             }
 
         # 
-        self.arrowImagesMap = {
+        self.arrowImagesMap_highlight = {
             2**0 : os.path.join(arrowBasePath, "1.png"),
             2**1 : os.path.join(arrowBasePath, "2.png"),
             2**2 : os.path.join(arrowBasePath, "4.png"),
@@ -58,6 +58,23 @@ class LaneShowImageDlg(QtGui.QDialog, FORM_CLASS):
             2**11 : os.path.join(arrowBasePath, "2048.png"),
             2**12 : os.path.join(arrowBasePath, "4096.png"),
             2**13 : os.path.join(arrowBasePath, "8192.png")            
+            }
+
+        self.arrowImagesMap_gray = {
+            2**0 : os.path.join(arrowBasePath, "1_gray.png"),
+            2**1 : os.path.join(arrowBasePath, "2_gray.png"),
+            2**2 : os.path.join(arrowBasePath, "4_gray.png"),
+            2**3 : os.path.join(arrowBasePath, "8_gray.png"),
+            2**4 : os.path.join(arrowBasePath, "16_gray.png"),
+            2**5 : os.path.join(arrowBasePath, "32_gray.png"),
+            2**6 : os.path.join(arrowBasePath, "64_gray.png"),
+            2**7 : os.path.join(arrowBasePath, "128_gray.png"),
+            2**8 : os.path.join(arrowBasePath, "256_gray.png"),
+            2**9 : os.path.join(arrowBasePath, "512_gray.png"),
+            2**10 : os.path.join(arrowBasePath, "1024_gray.png"),
+            2**11 : os.path.join(arrowBasePath, "2048_gray.png"),
+            2**12 : os.path.join(arrowBasePath, "4096_gray.png"),
+            2**13 : os.path.join(arrowBasePath, "8192_gray.png")            
             }
 
         for oneFeature in self.mFeatureList:
@@ -111,7 +128,36 @@ class LaneShowImageDlg(QtGui.QDialog, FORM_CLASS):
             lane_info = theFeature.attribute('lane_info')
             arrow_info = theFeature.attribute('arrow_info')
 
-            # get guide info lanes 
+            # draw all lanes in this link
+            strFilter = '''in_link_id=%s and node_id=%s and passlink_count=%s''' %\
+                        (in_link_id, node_id, passlink_count)
+            sqlcmd = """select lane_num, lane_info, arrow_info
+                        from %s
+                        where %s""" % (uri.table(), strFilter)
+            pg.execute(sqlcmd)
+            rows = pg.fetchall()
+            if len(rows) <= 0:
+                errMsg = '''get no lane record.'''
+                QMessageBox.information(self, "Show Lane", """error:\n%s"""%errMsg)
+                return
+            totalLaneCount = rows[0][0] # lane count of inlink.
+
+            self.clearAllGraphicViews() # clear all obsolete content in GraphicViews
+            self.hideAllGraphicViews()
+            self.showNeededGraphicViews(totalLaneCount)
+
+            for row in rows:
+                lane_num = row[0] # total lane count in this link
+                lane_info = row[1] # which lane is specified
+                arrow_info = row[2] # arrow catagory
+
+                if lane_num != totalLaneCount:
+                    errMsg = """some lane_count not the same."""
+                    QMessageBox.information(self, "Show Lane", """error:\n%s"""%errMsg)
+                    return
+                self.drawLaneGray(lane_num, lane_info, arrow_info)          
+
+            # draw highlighted guide lanes 
             strFilter = '''in_link_id=%s and node_id=%s and out_link_id=%s
                            and passlink_count=%s''' %\
                         (in_link_id, node_id, out_link_id, passlink_count)
@@ -126,11 +172,15 @@ class LaneShowImageDlg(QtGui.QDialog, FORM_CLASS):
                 return
             totalLaneCount = rows[0][0] # lane count of inlink.
             for row in rows:
-                if row[0] != totalLaneCount:
+                lane_num = row[0] # total lane count in this link
+                lane_info = row[1] # which lane is specified
+                arrow_info = row[2] # arrow catagory
+
+                if lane_num != totalLaneCount:
                     errMsg = """some lane_count not the same."""
                     QMessageBox.information(self, "Show Lane", """error:\n%s"""%errMsg)
                     return
-                self.drawLaneHighlight(totalLaneCount, 65535, 1)
+                self.drawLaneHighlight(lane_num, lane_info, arrow_info)
             return 
         except KeyError, kErr:
             errMsg = """Selected feature is not a rdb lane feature."""
@@ -150,16 +200,55 @@ class LaneShowImageDlg(QtGui.QDialog, FORM_CLASS):
         return strFeatureInfo
 
     def drawLaneHighlight(self, totalLaneCount, whichLane, arrowInfo):
-        pixmapList = []
-        for oneKey, oneValue in self.arrowImagesMap.items():
-            if arrowInfo & oneKey:
-                pixmapList.append(QPixmap(oneValue))
+        self.drawLane(totalLaneCount, whichLane, arrowInfo, self.arrowImagesMap_highlight)
+        return
 
-        if len(pixmapList) <= 0:
-            errMsg = "pixmapList=0, can find arrow picture."
+    def drawLaneGray(self, totalLaneCount, whichLane, arrowInfo):
+        self.drawLane(totalLaneCount, whichLane, arrowInfo, self.arrowImagesMap_gray)
+        return
+
+    def drawLane(self, totalLaneCount, whichLane, arrowInfo, arrowImagesMap):
+        if whichLane >= 2**(totalLaneCount+1): # specified lane not exist.
+            errMsg = """lane_info is incorrect, total lane count is %s""" % totalLaneCount
             QMessageBox.information(self, "Show Lane", """error:\n%s""" % errMsg)
             return
-        for oneKey, oneValue in self.labelsMap.items():
-            if whichLane & oneKey:
-                oneValue.setPixmap(pixmapList[0])
+
+        pixmapList = []
+        for oneKey, oneImagePath in arrowImagesMap.items():
+            if arrowInfo & oneKey:
+                pixmapList.append(QPixmap(oneImagePath))
+
+        if len(pixmapList) <= 0:
+            errMsg = "arrow_info is incorrect, cannot find arrow's picture."
+            QMessageBox.information(self, "Show Lane", """error:\n%s""" % errMsg)
+            return
+
+        for oneKey, oneGraphicView in self.graphicViewsMap.items():
+            if oneKey >= totalLaneCount:
+                continue
+            if whichLane & 2**(totalLaneCount-oneKey-1):
+                scene = QGraphicsScene()
+                for onePixmap in pixmapList:
+                    scene.addItem(QGraphicsPixmapItem(onePixmap))
+                oneGraphicView.setScene(scene)
+        return
+
+    # hide all graphic views.
+    def hideAllGraphicViews(self):
+        for oneKey, oneGraphicView in self.graphicViewsMap.items():
+            oneGraphicView.setVisible(False)
+        return
+
+    # show the graphic views that are needed.
+    def showNeededGraphicViews(self, totalLaneCount):
+        for oneKey, oneGraphicView in self.graphicViewsMap.items():
+            if oneKey < totalLaneCount:
+                oneGraphicView.setVisible(True)
+        return
+
+    # remove all content drawn in the graphic views.
+    def clearAllGraphicViews(self):
+        for oneKey, oneGraphicView in self.graphicViewsMap.items():
+            #oneGraphicView.clear()
+            continue
         return
