7f2ee96367cd3f0457cdf522d911d6cfeea82b10
tangpinghui <tangpinghui@pset.suntec.net>
2016-01-04 20:17:00 +0800

    feature version 1

--- Suntec/QgisPyPlugin/QtGraphicViewTest/RdbNewTick.py
+++ Suntec/QgisPyPlugin/QtGraphicViewTest/RdbNewTick.py
@@ -1,9 +1,20 @@
-﻿class RdbNewTick(object):
-    def __init__(self):
+﻿class RdbInformation(object):
+    def __init__(self, theP):
+        self.mTheP = theP
+        self.mTheVolumn = 0
+        self.mVolDistribution = {}
         return
 
-class RdbPool(object):
+class RdbInformationPool(object):
     def __init__(self):
+        self.mRdbInfoList = []
+        for i in range(1500, 2500):
+            self.mRdbInfoList.append(RdbInformation(i))
         return
+
+    def newTickCome(self, theTick):
+
+        return
+
 
 
